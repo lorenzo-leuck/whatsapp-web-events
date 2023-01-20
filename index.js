8ef56f23ab4fb5da253582f7741456339988c812
lorenzo-leuck <lorenzo.leuck@outlook.com>
2023-01-20 18:24:55 -0300

    wip

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const qrcode = require("qrcode-terminal");
 const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
 const { Buttons, Message } = require("whatsapp-web.js/src/structures");
 
+// ACK_ERROR: -1,
+// ACK_PENDING: 0,
+// ACK_SERVER: 1,
+// ACK_DEVICE: 2,
+// ACK_READ: 3,
+// ACK_PLAYED: 4,
+
+//     switch (message.ack) {
+//       case ACK_DEVICE:
+//         return info["delivery"][0].t;
+//       case ACK_READ:
+//         return info["read"][0].t;
+//       case ACK_PLAYED:
+//         return info["played"][0].t;
+//       default:
+//         return null;
+//     }
+//   };
+
 const clientPathFolder = {
+  // clientId: "10" + "-" + "555181559667",
   clientId: "10" + "-" + "5551982211460",
   dataPath: "./.wwebjs_auth",
 };
@@ -11,6 +31,16 @@ const client = new Client({
   authStrategy: new LocalAuth(clientPathFolder),
   puppeteer: { headless: true },
 });
+
+const buttons_reply_url = new Buttons(
+  "teste ios",
+  [
+    { body: "Test", id: "test-1" },
+    { body: "Test 2", url: "https://wwebjs.dev" },
+  ],
+  "title",
+  "footer"
+); // Reply button with URL
 
 function authenticate() {
   client.on("qr", (qr) => {
@@ -21,25 +51,59 @@ function authenticate() {
 client.initialize();
 
 client.on("ready", async () => {
+  // iphone
+  // const number = await client.getNumberId("555184498643");
+  // android
   const number = await client.getNumberId("555181559667");
+  // const number = await client.getNumberId("5551982211460");
 
   await client.sendMessage(number._serialized, "teste");
 });
 
-// client.on("message_create", async (msg) => {
-//   let chat = await msg.getInfo();
+client.on("message_create", async (msg) => {
+  // console.log("----------------msg_info------------------");
+  // console.log(msg);
+  // console.log("------------------------------------------");
 
-//   console.log("----------------msg_info------------------");
-//   console.log(chat);
-//   console.log("------------------------------------------");
-// });
+  setInterval(() => {
+    let info = msg.getInfo();
+
+    console.log("----------------msg_info------------------");
+    console.log(info);
+    console.log("------------------------------------------");
+  }, 1000);
+});
 
 client.on("message_ack", async (msg) => {
-  console.log("----------------msg_ack------------------");
-  console.log(msg.to);
-  console.log(msg.ack);
-  console.log("------------------------------------------");
+  let info = await msg.getInfo();
+
+  setInterval(() => {
+    console.log("----------------msg_ack------------------");
+
+    contato = {};
+
+    contato[msg.to] = msg.ack;
+    //   let info = await msg.getInfo();
+
+    console.log(info);
+    console.log("------------------------------------------");
+  }, 1000);
 });
+
+// client.on("message", async (msg) => {
+//   console.log("----------------msg_response------------------");
+//   console.log(msg);
+//   console.log("------------------------------------------");
+//   // console.clear();
+
+//   if (msg.body === "Test") {
+//     let message = "https://app.fidelizou.me/a/2989/";
+
+//     // message.links([{ link: "www.youtube.com", isSuspicious: false }]);
+
+//     client.sendMessage(msg.from, message);
+//   }
+// });
 
 // client.on("message_create", async (msg) => {
 //   const info = await msg.getInfo();
